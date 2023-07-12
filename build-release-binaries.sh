13b16047e969b5f12b71ab9f1bb0eeb69a769ecb
D. Bohdan <dbohdan@dbohdan.com>
2023-07-12 10:25:47 +0000

    build(release): update script

--- build-release-binaries.sh
+++ build-release-binaries.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-make release
-cp "/tmp/$USER/cargo/csv2html/x86_64-unknown-linux-musl/release/csv2html" csv2html-linux-x86_64
+cargo build --release
+cp target/release/csv2html csv2html-linux-x86_64
 strip csv2html-linux-x86_64
 
-RUSTFLAGS="-C panic=abort -C lto" make release TARGET=i686-pc-windows-gnu
-cp "/tmp/$USER/cargo/csv2html/i686-pc-windows-gnu/release/csv2html.exe" csv2html-win32.exe
+cargo build --release --target i686-pc-windows-gnu
+cp target/i686-pc-windows-gnu/release/csv2html.exe csv2html-win32.exe
 strip csv2html-win32.exe
